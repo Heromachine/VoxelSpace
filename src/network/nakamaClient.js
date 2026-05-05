ce5af8a183846a64f74509d8f5d63e3221ab34ff
user.email <jessie_reyna@live.com>
2026-05-05 07:40:02 -0500

    Auto-update Cloudflare Tunnel URL for Nakama server

--- src/network/nakamaClient.js
+++ src/network/nakamaClient.js
@@ -5,7 +5,7 @@
 
 var NakamaClient = (function () {
 
-    var SERVER_HOST = "foods-physical-milwaukee-articles.trycloudflare.com";
+    var SERVER_HOST = "inspection-commentary-restoration-wallace.trycloudflare.com";
     var SERVER_PORT = "443";
     var SERVER_KEY  = "1eebcf80e66915369084f9fa";
     var USE_SSL     = true;
