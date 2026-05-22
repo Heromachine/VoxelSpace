9f5134d6cc9d2a6c266d8e31fca45d6c26ff084b
user.email <jessie_reyna@live.com>
2026-05-22 07:35:55 -0500

    Update Cloudflare tunnel URL -> ash-browsing-masters-yet.trycloudflare.com

--- src/network/nakamaClient.js
+++ src/network/nakamaClient.js
@@ -5,7 +5,7 @@
 
 var NakamaClient = (function () {
 
-    var SERVER_HOST = "displays-entitled-jack-delivering.trycloudflare.com";
+    var SERVER_HOST = "ash-browsing-masters-yet.trycloudflare.com";
     var SERVER_PORT = "443";
     var SERVER_KEY  = "1eebcf80e66915369084f9fa";
     var USE_SSL     = true;
