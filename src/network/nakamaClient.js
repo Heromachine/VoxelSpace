ae83d9f20ff5255b81712b589bc95e2efe87db09
user.email <jessie_reyna@live.com>
2026-05-26 07:35:57 -0500

    Update Cloudflare tunnel URL -> electronics-roster-thompson-marie.trycloudflare.com

--- src/network/nakamaClient.js
+++ src/network/nakamaClient.js
@@ -5,7 +5,7 @@
 
 var NakamaClient = (function () {
 
-    var SERVER_HOST = "ordered-clinics-things-obituaries.trycloudflare.com";
+    var SERVER_HOST = "electronics-roster-thompson-marie.trycloudflare.com";
     var SERVER_PORT = "443";
     var SERVER_KEY  = "1eebcf80e66915369084f9fa";
     var USE_SSL     = true;
