109d5d3d43027003d4a72abb5c90751af1cd54f2
user.email <jessie_reyna@live.com>
2026-05-30 07:35:55 -0500

    Update Cloudflare tunnel URL -> singles-sustained-blind-operations.trycloudflare.com

--- src/network/nakamaClient.js
+++ src/network/nakamaClient.js
@@ -5,7 +5,7 @@
 
 var NakamaClient = (function () {
 
-    var SERVER_HOST = "vehicle-aims-rides-requires.trycloudflare.com";
+    var SERVER_HOST = "singles-sustained-blind-operations.trycloudflare.com";
     var SERVER_PORT = "443";
     var SERVER_KEY  = "1eebcf80e66915369084f9fa";
     var USE_SSL     = true;
