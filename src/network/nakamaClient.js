b42039c96059ecfe3c701272075dd477cf5dc065
user.email <jessie_reyna@live.com>
2026-06-05 07:35:55 -0500

    Update Cloudflare tunnel URL -> garden-corners-printed-terrace.trycloudflare.com

--- src/network/nakamaClient.js
+++ src/network/nakamaClient.js
@@ -5,7 +5,7 @@
 
 var NakamaClient = (function () {
 
-    var SERVER_HOST = "liabilities-permitted-interviews-flat.trycloudflare.com";
+    var SERVER_HOST = "garden-corners-printed-terrace.trycloudflare.com";
     var SERVER_PORT = "443";
     var SERVER_KEY  = "1eebcf80e66915369084f9fa";
     var USE_SSL     = true;
