7c07af56f102bc9e81bf3beffa8f702d2215811c
user.email <jessie_reyna@live.com>
2026-06-13 07:35:52 -0500

    Update Cloudflare tunnel URL -> got-decades-normally-moms.trycloudflare.com

--- src/network/nakamaClient.js
+++ src/network/nakamaClient.js
@@ -5,7 +5,7 @@
 
 var NakamaClient = (function () {
 
-    var SERVER_HOST = "wales-ones-house-neural.trycloudflare.com";
+    var SERVER_HOST = "got-decades-normally-moms.trycloudflare.com";
     var SERVER_PORT = "443";
     var SERVER_KEY  = "1eebcf80e66915369084f9fa";
     var USE_SSL     = true;
