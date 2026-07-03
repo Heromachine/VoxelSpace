af497a4bacc778e6bedef539b2b59227f806a6c7
user.email <jessie_reyna@live.com>
2026-07-03 07:35:49 -0500

    Update Cloudflare tunnel URL -> struct-bestsellers-gradually-internal.trycloudflare.com

--- src/network/nakamaClient.js
+++ src/network/nakamaClient.js
@@ -5,7 +5,7 @@
 
 var NakamaClient = (function () {
 
-    var SERVER_HOST = "alice-revised-lately-les.trycloudflare.com";
+    var SERVER_HOST = "struct-bestsellers-gradually-internal.trycloudflare.com";
     var SERVER_PORT = "443";
     var SERVER_KEY  = "1eebcf80e66915369084f9fa";
     var USE_SSL     = true;
